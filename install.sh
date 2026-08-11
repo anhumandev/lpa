ec56014ac5e5635422ff13ad0e6a67a8653e020f
Pouya <pouya.momhidei@gmail.com>
2026-08-11 18:02:34 +0330

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+wget https://github.com/pouya1dev/lpa/releases/download/alpha-0.0.3/lpa-x86_64-linux-v0.0.3alpha.tar.xz
+tar -xJvf lpa-x86_64-linux-v0.0.3alpha.tar.xz
+mv lpa-x86_64-linux-v0.0.3alpha resource_p
+cd resource_p
+chmod +x install.sh
+sudo install.sh
