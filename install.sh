10a672add76c97031b20432e7bdba631c3dfa365
pouya1dev <291070369+pouya1dev@users.noreply.github.com>
2026-08-21 22:16:53 +0330

    fix installer

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 wget https://github.com/ekolang/eko/archive/refs/tags/alpha-0.0.4.zip
 mkdir alpha-0.0.4
 unzip alpha-0.0.4.zip -d alpha-0.0.4/
-cd alpha-0.0.4/src/runtime
+cd alpha-0.0.4/eko-alpha-0.0.4/src/runtime
 dub
+sudo mv libmarschiert.a /usr/local/lib
 cd ..
 dub
