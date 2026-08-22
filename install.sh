b804bfa210870aab2b28f4ff86180b5e8fe206cc
pouya1dev <291070369+pouya1dev@users.noreply.github.com>
2026-08-22 15:46:26 +0330

    Installer tested and bugs fixed + new progress message

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 echo "eko Installer - version 0.0.1"
 wget https://github.com/ekolang/eko/archive/refs/tags/alpha-0.0.4.zip
-mkdir alpha-0.0.4
+if [ -d "alpha-0.0.4"  ]; then
+	echo "Remove old install progress cache and files for clean install?"
+	echo "[Y][N]"
+	read answer
+	if [ "$answer" == "Y" ]; then
+		rm -r alpha-0.0.4
+		mkdir alpha-0.0.4
+	else
+		echo "Continue without remove anything."
+	fi
+else
+	mkdir alpha-0.0.4
+fi
 unzip alpha-0.0.4.zip -d alpha-0.0.4/
 cd alpha-0.0.4/eko-alpha-0.0.4/src/runtime
 dub
@@ -11,3 +23,16 @@ dub
 sudo mv libaxiom.a /usr/local/lib
 cd ..
 dub
+if [ -f "eko" ]; then
+	echo -e "\033[32mBuild Done. do you want to move it to usr/local/bin?"
+	echo -e "\033[0m[Y][N]"
+	read ans
+	if [ "$ans" == "Y" ]; then
+		sudo mv eko /usr/local/bin
+		echo "progress finished. (2)BuildDone."
+	else
+		echo "progress finished. (1)BuildDone."
+	fi
+else
+	echo "Faild To compile eko. (0)Build."
+fi
