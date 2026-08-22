22dee09cd4e67c3bbc3b8d1a950eb010bb5352d6
pouya1dev <291070369+pouya1dev@users.noreply.github.com>
2026-08-22 15:30:59 +0330

    Fix Bug found in Installer: it wont move axiom after build so linker cant find it.

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
+echo "eko Installer - version 0.0.1"
 wget https://github.com/ekolang/eko/archive/refs/tags/alpha-0.0.4.zip
 mkdir alpha-0.0.4
 unzip alpha-0.0.4.zip -d alpha-0.0.4/
 cd alpha-0.0.4/eko-alpha-0.0.4/src/runtime
 dub
 sudo mv libmarschiert.a /usr/local/lib
+cd ..
+cd axiom
+dub
+sudo mv libaxiom.a /usr/local/lib
 cd ..
 dub
